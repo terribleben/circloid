822f5f5646c8f5c18b18f2e59a895b1e55d894f5
schazers <schazers@gmail.com>
2018-08-08 20:12:41 -0700

    added sounds

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ Target = require 'target'
 Particles = require 'particles'
 Timer = require 'timer'
 Menu = require 'menu'
+require 'TESound'
 
 Circloid = {
    _radiusToDraw = 0,
@@ -15,6 +16,18 @@ Circloid = {
 }
 
 function love.load()
+  -- below 2 lines don't work. soundData loading is bugged?
+  --snd_on_game_loaded = love.sound.newSoundData("on_game_loaded.mp3")
+  --snd_title_screen_loop = love.sound.newSoundData("title_screen_loop.mp3")
+
+  -- can use below sources directly if needed, but TESound doesn't allow it
+  --snd_on_game_loaded = love.audio.newSource("on_game_loaded.mp3", "static")
+  --snd_title_screen_loop = love.audio.newSource("title_screen_loop.mp3", "static")
+  --snd_on_game_start = love.audio.newSource("on_game_start.mp3", "static")
+  --snd_main_loop = love.audio.newSource("main_loop.mp3", "stream")
+
+  --TEsound.stop("on_game_loaded")
+
    _loadFont()
    gDrawFuncs = {
       ["init"] = Menu.draw,
@@ -35,12 +48,16 @@ function love.keypressed(key, scancode, isrepeat)
    Background:keypressed(key)
    if key == "a" then
       Player:rotateCounter()
+      TEsound.play("move.mp3", "static")
    elseif key == "d" then
       Player:rotateClockwise()
+      TEsound.play("move.mp3", "static")
    elseif key == "w" then
       Player:addRay()
+      TEsound.play("size_up.mp3", "static")
    elseif key == "s" then
       Player:subtractRay()
+      TEsound.play("size_down.mp3", "static")
    end
 
    if GameState.state ~= "game" then
@@ -78,6 +95,7 @@ function love.update(dt)
          _turnFailed()
       end
    end
+   TEsound.cleanup()
 end
 
 function _turnFailed()
@@ -87,11 +105,13 @@ function _turnFailed()
    GameState:turnFailed()
    Particles:redBurst()
    Circloid:shakeCamera()
+   TEsound.play("take_damage.mp3", "static")
    if GameState.vitality <= 0 then
       if GameState.state ~= "end" then
          GameState.state = "end"
          HiScore:maybeSave(GameState.score)
       end
+      TEsound.play("game_over.mp3", "static")
       _resetGame()
    elseif GameState.vitality <= 1 then
       Background:setMessage("CRITICAL", { isPersistent = true, ttl = 5 })
@@ -109,6 +129,7 @@ function _turnSucceeded()
    Circloid._scale = 1.1
    Particles:greenBurst()
    Particles:playerMatched(Player.rayPosition, Player.rayCount)
+   TEsound.play("zap.mp3", "static")
    if GameState.vitality == GameState.MAX_VITALITY then
       Background:setMessage("MAXIMUM VITALITY")
    else
@@ -121,14 +142,14 @@ function _drawGame()
    local bigFont = GameState.font[48]
    love.graphics.setFont(bigFont)
    love.graphics.translate(Circloid._cameraOffset.x, Circloid._cameraOffset.y)
-   
+
    love.graphics.setColor(1, 1, 1, 1)
    local centerX = GameState.viewport.width * 0.5
    local centerY = GameState.viewport.height * 0.5
    local radius = Circloid._radiusToDraw
    local scoreStr = tostring(GameState.score)
    local scoreWidth = bigFont:getWidth(scoreStr)
-   
+
    love.graphics.push()
    love.graphics.translate(centerX, centerY)
    love.graphics.print(scoreStr, -scoreWidth * 0.5, -bigFont:getHeight() * 0.5)
@@ -151,11 +172,26 @@ function _resetGame()
    GameState:reset()
    Timer:reset()
    Circloid:reset()
+
+   TEsound.stop("on_game_start")
+   TEsound.stop("main_loop")
+   TEsound.play("on_game_loaded.mp3", "static", "on_game_loaded", 1, 1,
+     function(d)
+       TEsound.playLooping("title_screen_loop.mp3", "static", "title_screen_loop")
+     end
+   )
 end
 
 function _restartGame()
    Background:setMessage("BEGIN", { isPersistent = true, ttl = 2 })
    GameState.state = "game"
+   TEsound.stop("on_game_loaded")
+   TEsound.stop("title_screen_loop")
+   TEsound.play("on_game_start.mp3", "static", "on_game_start", 1, 1,
+     function(d)
+       TEsound.playLooping("main_loop.mp3", "stream", "main_loop")
+     end
+   )
 end
 
 function _loadFont()
