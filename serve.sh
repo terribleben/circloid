f988fb5e85d5979cc4453fb48d17b90514a5a852
Ben Roth <spongebucket@gmail.com>
2019-01-15 14:36:25 -0800

    add .castle metadata

--- serve.sh
+++ serve.sh
@@ -7,7 +7,7 @@ python -m SimpleHTTPServer &
 sleep 0.5
 
 if [[ "$OSTYPE" == "darwin" ]]; then
-    open castle://localhost:8000/main.lua
+    open castle://localhost:8000/circloid.castle
 fi
 
 wait
