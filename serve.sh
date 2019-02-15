c13e3433dde69dd4beb9199b836b3f1713d74c89
Ben Roth <spongebucket@gmail.com>
2019-02-15 09:48:43 -0800

    updating primary color

--- serve.sh
+++ serve.sh
@@ -6,7 +6,7 @@ trap "kill 0" EXIT
 python -m SimpleHTTPServer &
 sleep 0.5
 
-if [[ "$OSTYPE" == "darwin" ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
     open castle://localhost:8000/circloid.castle
 fi
 
